b30a98b75fd9bc6c7a109edca6c192e88e0f806e
BernardTatin <bernard.tatin@outlook.fr>
2016-04-04 00:07:33 +0200

    First test : it works!

---
+++ bin/oaklisp.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# 
+# File:   oaklisp.sh
+# Author: bernard
+#
+# Created on 3 avr. 2016, 23:58:05
+#
+
+OAKPATH="/export/home/bernard/Documents/Histoire de l'informatique/oaklisp-20-jul-93"
+
+"${OAKPATH}/src/src/emulator" "${OAKPATH}/lib/oaklisp.lo"
